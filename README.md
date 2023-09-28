fa0c527893bbdc9d93cddb2a49381b7e287df814
alhadrad@gmail.com <akoerner>
2023-09-29 00:50:34 +0200

    added numberical buffers, updated readme, updated help

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 # cdbuff
 Your grandpa's cd command
 
-cdbuff is a simple command line tool to enhance the 'cd' command written in bash
-to save and recall paths by name. It can be useful if you have multiple projects
-where you can store the path of each project workspace with cdbuff in "named 
-buffers".
+cdbuff is a simple command line tool to enhance the 'cd' command written in 
+bash. It can be useful if you have multiple projects allowing you to store or 
+recall paths by name or index. 
+
+cdbuff is meant to be used interactively to quickly change your working
+directory to and from frequently used directories.
+
+## Named Buffers
+With cdbuff you can save and restore paths by assigning arbitrary names to them.
+
+## Rolling Indexed Buffers
+cdbuff has a circular buffer similar to vim.  Every path stored with '-s' will
+appear in the indexed buffers. With each new path the index of the previous is 
+incremented.
 
 ## Getting started
 1. clone the repo
@@ -12,34 +22,26 @@ buffers".
 git clone https://github.com/akoerner/cdbuff.git
 ```
 
-2. in your bashrc or zshrc source cdbuff
+2. In your .bashrc or .zshrc source cdbuff:
 ```bash
-...
 source path/to/cdbuff/cdbuff
-...
+alias cb=cdbuff
 ```
 3. Set the primary buffer:
 ```bash
 cd to/some/interesting/path
 cdbuff -s
+Setting cd buffer: (primary): /home/cdbuff/to/some/interesting/path
 ```
 
+Later you can recall and change back to that directory stored in the `primary`
+buffer by invoking `cdbuff` with no flags: 
 ```bash
-cdbuff -s
+cdbuff
+Changing directory to: primary@/home/cdbuff/to/some/interesting/path
+/home/cdbuff/to/some/interesting/path
 Setting cd buffer: (primary): /home/cdbuff/this/is/an/interesting/path
 ```
-
-4. Later return to the primary buffer path:
-```bash
-pwd
-/home/cdbuff
-cdbuff
-Changing directory to: primary@/home/cdbuff/this/is/an/interesting/path
-/home/cdbuff/this/is/an/interesting/path
-pwd
-/home/cdbuff/this/is/an/interesting/path
-```
-
 
 #### List available buffers
 The following command will return a list of all defined cdbuff buffers:
@@ -49,56 +51,67 @@ cdbuff -l
 
 #### Setting a named cd buffer
 1. cd to a path you want to store
-2. invoke cdbuff with -s for set and -b to define the buffer:
+2. invoke cdbuff with `-s`:
 ```bash
 cd some/path
-cdbuff -s -b special_path
+cdbuff -s special_path
 Setting cd buffer: (special_path): /home/cdbuff/some/path
-
 ```
 
 #### Returning to a named buffer
 You have two options to return to a previously named buffer. The first is to 
 simply use the buffer name:
 ```bash
-cdbuff -b special_path
+cdbuff special_path
 ```
+
 The second option is to use the buffer index:
 1.
 ```bash
 cdbuff -l
-Named buffers:
-    0: (primary)  : /home/cdbuff/this/is/an/interesting/path         => 'cdbuff primary' or 'cdbuff 0' or 'cdbuff -b primary'                        
-    1: (special_path): /home/cdbuff/some/path                        => 'cdbuff special_path' or 'cdbuff 1' or 'cdbuff -b special_path'              
+```
+```bash
+Numerical buffers:
+    9:
+    8:
+    7:
+    6:
+    5:
+    4:
+    3:
+    2: 
+    1:
+    0: /home/cdbuff/some/path
+
+Literal buffers:
+   (fas): /home/cdbuff/some/path
+
     buffer file: /home/cdbuff/.cdbuff
 ```
 
+> **ℹ️INFO:**
+> The buffer index will automatically advance with each invocation of `cdbuff -s`
+
 2. Once you know the index you can always use it to refer to the buffer:
 ```bash
-cdbuff 1
+cdbuff 0 
 Changing directory to: special_path@/home/cdbuff/some/path
 /home/cdbuff/some/path
 ```
 
-**Note:** Indexes are dynamic and will change with any modification of the named
-buffers. Be sure to use "cdbuff -l" to verify the new indexes.
-
 #### Deleting a named buffer
-Using the '-d' flag will delete the named buffer.
-The following command will delete the "special_path" buffer which in this
-example is index '1'
+Using the '-d' flag will delete a buffer.
+The following command will delete numerical buffer which in this
+example is index '1':
 ```bash
-cdbuff -d -b 1
+cdbuff -d 1
+Deleted: 1@/home/cdbuff/some/path
+```
+You can also delete named buffers:
+```bash
+cdbuff -d special_path 
 Deleted: special_path@/home/cdbuff/some/path
 ```
-You can also of course refer to the buffer by name:
-```bash
-cdbuff -d -b special_path 
-Deleted: special_path@/home/cdbuff/some/path
-```
-Note that indexes are dynamic and subject to change whenever adding or deleting
-buffers so always double check the index after updating the buffers with '-l'
-
 
 #### cdbuff buffer file
 The cdbuff buffer file is $HOME/.cdbuff by default
--- cdbuff.sh
+++ cdbuff.sh
@@ -22,38 +22,43 @@ cdbuff_file="${CDBUFF_FILE:-$DEFAULT_BUFFER_FILE}"
 _help() {
     cat << EOF 
 NAME
-    cdbuff - cd but with memory 
+    cdbuff - cd but with memory and history 
 
     Grandpa's cd with buffers, less typey more workey
 
 SYNOPSIS
   USAGE
-    cdbuff -s             Set the 'primary' buffer to the current working directory
-    cdbuff -d -b primary  Delete the primary buffer
-    cdbuff -d -b 3        Delete buffer at index 3, to list indexes use the -l flag
-    cdbuff -s -b test     Create a new buffer called 'test' and save the current working directory 
-    cdbuff -b test        Change directory to the path stored with the 'test' buffer
+    cdbuff -s             Set the 'primary' named buffer to the current working directory
+    cdbuff                Change directory to path stored in the  'primary' named buffer
+ 
+    cdbuff test           Change directory to the path stored with the 'test' buffer
+    cdbuff -s test        Create a new buffer called 'test' and save the current working directory 
+    cdbuff -d primary     Delete the primary buffer
+    cdbuff -d 3           Delete buffer at index 3, to list indexes use the -l flag
     cdbuff -l             List all defined buffers
     cdbuff -p             Print the primary buffer and exit 
 
 DESCRIPTION
-    cdbuffer is an enhancement to the 'cd' command adding named buffers that can
-    be saved and restored on-demand. Duck and weave through your file system 
-    like a ninja. The features include:
+    cdbuffer is an enhancement to the 'cd' command adding named buffers and a 
+    circular index buffer, similar to vim, that can be saved and restored 
+    on-demand. Duck and weave through your file system like a ninja. The 
+    features include:
     - Saving directories as a named buffer
-    - cd'ing to named buffers
+    - Vim like circular buffer 
+    - cd'ing to named and indexed buffers
     - list available buffers
     - managing named buffers i.e., deleting them
+    - 
 
 OPTIONS
 
     -h, --help             Print this help and exit
     -f, --buffer-file      CD buffer file to save path data. 
                              Default: "${DEFAULT_BUFFER_FILE}"
-    -b, --buffer           Buffer name or index (use: cdbuff -l to discover buffer indices)
     -d, --delete           Delete named buffer
-    -c, --cd               Change directory to project path (if no other flags are provided this is implied and the default behavior)
-    -p, --print            Print the buffer and exit 
+    -p, --print            Print the `primary` buffer and exit 
+    -n, --nuke             Delete all buffers in the buffer file 
+    -D, --dump             cat the buffer file 
     -v, --verbose          verbose output
 
     The default buffer is always "${DEFAULT_BUFFER}" unless specified with -b <buffer name> or --buffer <buffer name>
@@ -61,27 +66,98 @@ EOF
     exit
 } 
 
+confirm() {
+    while true; do
+        read -p "All buffers in the buffer file: ${buffer_file} will be deleted. Do you want to continue? (y/n): " choice
+        case "$choice" in
+            [Yy]* ) return 0;;
+            [Nn]* ) return 1;;
+            * ) echo "Please answer 'yes' or 'no'.";;
+        esac
+    done
+}
+
 list_buffers(){
     buffer_file="${1}"
 
     if [[ ! -f "${buffer_file}" ]]; then
         exiterr "    ERROR: cd buffer file: ${buffer_file} does not exist."
     fi
-
-    buffers="$(cat "${buffer_file}")"
+    numerical_buffers="$(cat ${buffer_file} | sed -n '/^[0-9]@/p')"
+    literal_buffers="$(cat "${buffer_file}" | sed -n '/^[0-9]@/!p')"
     index=0
-    if [[ -n "$buffers" ]]; then
-        echo $(bold "Named buffers:")
+    echo $(bold "Numerical buffers:")
+    if [[ -n "$numerical_buffers" ]]; then
         while IFS= read -r buffer_line; do
             buffer=$(echo "$buffer_line" | cut -d '@' -f 1)
             path=$(echo "$buffer_line" | cut -d '@' -f 2-)
-            printf "    %s: %-30s: %-45s %-80s\n" "$(bold "${index}")" "($(emphasis ${buffer} "green"))" "${path}" "=> 'cdbuff ${buffer}' or 'cdbuff ${index}' or 'cdbuff -b ${buffer}'"
+            printf "    %s: %-30s\n" "$(bold "${buffer}")" "${path}"
+        done <<< "$numerical_buffers"
+    fi
+    echo
+    echo $(bold "Literal buffers:")
+    if [[ -n "$literal_buffers" ]]; then
+        while IFS= read -r buffer_line; do
+            buffer=$(echo "$buffer_line" | cut -d '@' -f 1)
+            path=$(echo "$buffer_line" | cut -d '@' -f 2-)
+            #printf "    %s: %-30s: %-45s %-80s\n" "$(bold "${index}")" "($(emphasis ${buffer} "green"))" "${path}" "=> 'cdbuff ${buffer}' or 'cdbuff ${index}' or 'cdbuff -b ${buffer}'"
+            printf "    %s: %-50s \n" "($(emphasis ${buffer} "green"))" "${path}"
             ((index=index+1))
-        done <<< "$buffers"
+        done <<< "$literal_buffers"
     else
-        exiterr "$(emphasis "ERROR:" "red") no named buffers in buffer file: ${buffer_file}. Call 'cdbuff -s' or 'cdbuff -s -p some_name' to set a named buffer."
+        echo "$(bold "INFO:") No named buffers in buffer file: ${buffer_file}. Call 'cdbuff -s' to set a named buffer."
     fi
     printf "    %s %s\n" "$(emphasis "buffer" 'red') $(emphasis "file: " 'red')" "${buffer_file}"
+}
+
+numerical_buffers_clear(){
+    local buffer_file="${1}"
+    
+    if [[ ! -f "${buffer_file}" ]]; then
+        exiterr "    ERROR: cd buffer file: ${buffer_file} does not exist."
+    fi
+   
+    sed -i -n '/^[0-9]@/p' "${buffer_file}"
+
+}
+
+numerical_buffers_init(){
+    local buffer_file="${1}"
+    
+    if [[ ! -f "${buffer_file}" ]]; then
+        exiterr "    ERROR: cd buffer file: ${buffer_file} does not exist."
+    fi
+
+
+    numerical_buffers="$(cat ${buffer_file} | sed -n '/^[0-9]@/p')"
+    if [[ -z "${numerical_buffers}" ]]; then
+        numerical_buffers_clear "${buffer_file}"
+        for ((index=0; index<=9; index++)); do
+            printf "%s@\n" "${index}" >> "${buffer_file}"
+        done
+    fi
+}
+
+numerical_buffer_push(){
+    path="${1}"
+    buffer_file="${2}"
+    
+    if [[ ! -f "${buffer_file}" ]]; then
+        exiterr "    ERROR: cd buffer file: ${buffer_file} does not exist."
+    fi
+
+    numerical_buffers="$(cat ${buffer_file} | sed -n '/^[0-9]@/p')"
+    numerical_buffers=$(echo "$numerical_buffers" | awk -F'@' '{print $1+1 "@" $2}')
+    numerical_buffers=$(echo "$numerical_buffers" | awk -F'@' '$1 <= 10')
+
+    for ((index=9; index>=1; index--)); do
+        buffer=$index
+        temp_path=$(echo "${numerical_buffers}" | grep "${buffer}@" | cut -d "@" -f2)
+        if [ -n "$temp_path" ]; then
+            cd_buffer_set "${buffer}" "${buffer_file}" "${temp_path}"
+        fi
+    done
+    cd_buffer_set "0" "${buffer_file}" "${path}"
 
 }
 
@@ -94,12 +170,13 @@ cd_buffer(){
         exiterr "ERROR: no cd buffers set!"
     fi
 
-    if [[ $buffer =~ ^[0-9]+$ ]]; then
-        ((buffer=buffer+1))
-        buffer_line="$(cat "${buffer_file}" | sed "${buffer}q;d")"
-    else
-        buffer_line="$(grep -E "^${buffer}@" "${buffer_file}" || echo "")"
-    fi
+
+    #if [[ $buffer =~ ^[0-9]+$ ]]; then
+    #    ((buffer=buffer+1))
+    #    buffer_line="$(cat "${buffer_file}" | sed "${buffer}q;d")"
+    #else
+    buffer_line="$(grep -E "^${buffer}@" "${buffer_file}" || echo "")"
+    #fi
 
     if [[ -z "$buffer_line" ]]; then
         exiterr "ERROR: no cd buffer for buffer: ${buffer} found in: ${buffer_file}"
@@ -137,14 +214,20 @@ buffer_print(){
 cd_buffer_set(){
     local buffer="${1}"
     local buffer_file="${2}"
+    local path="${3:-}"
     
     if [[ ! -f "${buffer_file}" ]]; then
         exiterr "ERROR: cd buffer file: ${buffer_file} does not exist."
     fi
-    cwd="$(pwd)"
-    echo "$(bold "Setting cd buffer:") ($(emphasis "${buffer}" "green")): ${cwd}"
+
+    if [[ -z "${path}" || "${path}" =~ ^[[:space:]]+$ ]]; then
+        path="$(pwd)"
+    fi
+    if ! [[ "$buffer" =~ ^[0-9]+$ ]]; then
+        echo "$(bold "Setting cd buffer:") ($(emphasis "${buffer}" "green")): ${path}"
+    fi
     sed -i "/^${buffer}\b/d" "${buffer_file}"
-    printf "\n%s@%s\n" "${buffer}" "${cwd}" >> "${buffer_file}"
+    printf "%s@%s\n" "${buffer}" "${path}" >> "${buffer_file}"
 }
 
 cd_buffer_sort(){
@@ -214,13 +297,15 @@ emphasis () {
 buffer_file=
 buffer=
 
-function parse_params() {
+parse_params() {
   buffer="${DEFAULT_BUFFER}"
-  buffer_file="${cdbuff_file}"
+  buffer_file="${DEFAULT_BUFFER_FILE}"
   list=0
   set_buffer=0
   _cd=0
   delete=0
+  dump=0
+  nuke=0
   print=0
 
   
@@ -238,18 +323,24 @@ function parse_params() {
     case "${1-}" in
     -h | --help) echo "$(_help)" | less ;;
     -v | --verbose) set -x ;;
-    -b | --buffer)
-      buffer="${2-}"
-      shift
-      ;;
     -f | --buffer-file) # example named parameter
       buffer_file="${2-}"
       shift
       ;;
     -l | --list-buffers) list=1;;
-    -s | --set-buffer) set_buffer=1;;
+    -s | --set-buffer) 
+      set_buffer=1
+      buffer="${2-}"
+      shift
+      ;;
     -c | --cd) _cd=1;;
-    -d | --delete) delete=1;;
+    -d | --delete) 
+      delete=1
+      buffer="${2-}"
+      shift
+      ;;
+    -D | --dump) dump=1;;
+    -n | --nuke) nuke=1;;
     -p | --print) print=1;;
     -?*) exiterr "ERROR: Unknown option: $1" ;;
     *) break ;;
@@ -259,12 +350,25 @@ function parse_params() {
 
     args=("$@")
 
-    # check required params and arguments
     [[ -z "${buffer-}" ]] && exiterr "ERROR: no buffer provided."
-    #  [[ ${#args[@]} -eq 0 ]] && die "Missing script arguments"
 
     touch ${buffer_file}
     #echo "  Buffer: ${buffer}"
+    if [[ "${nuke}" -eq 1 ]]; then
+        if confirm; then
+           cat /dev/null > ${buffer_file}
+           echo "cdbuff buffer file nuked: ${buffer_file}"
+           
+        else
+            echo "Your buffer file has been spared."
+        fi 
+    fi
+    if [[ "${dump}" -eq 1 ]]; then
+        echo "cdbuff file: ${buffer_file}"
+        echo ""
+        cat "${buffer_file}"
+        exit 0
+    fi
     if [[ "${print}" -eq 1 ]]; then
         buffer_print "${buffer}" "${buffer_file}"
         exit 0
@@ -274,10 +378,12 @@ function parse_params() {
     fi
     if [[ "${set_buffer}" -eq 1 ]]; then
         cd_buffer_set "${buffer}" "${buffer_file}"
-        cd_buffer_sort "${buffer_file}"
+        #cd_buffer_sort "${buffer_file}"
+        numerical_buffer_push "$(pwd)" "${buffer_file}"
     fi
     if [[ "${_cd}" -eq 1 ]]; then
         cd_buffer "${buffer}" "${buffer_file}"
+        exit 0
     fi
 
     if [[ $list -eq 1 && $set_buffer -eq 1 && $_cd -eq 1 && -z $buffer ]]; then
@@ -288,6 +394,8 @@ function parse_params() {
         cd_buffer_delete "${buffer}" "${buffer_file}"
         cd_buffer_sort "${buffer_file}"
     fi
+
+    numerical_buffers_init "${buffer_file}"
     return 0
 }
 parse_params "$@"
